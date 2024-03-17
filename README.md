b1ca0f2cff08d77efe8a54e50803ef5d94aed7ec
Ruminov <74424348+Ruminov@users.noreply.github.com>
2024-03-17 02:00:55 -0400

    Create index.html script.js and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock-Paper-Scissors
+
+A game made from scratch to test my problem solving skills.
---
+++ script.js
@@ -0,0 +1,42 @@
+function getComputerChoice() {
+  const number = Math.floor(Math.random() * 3) + 1;
+  if (number === 1) {
+    return "Rock";
+  } else if (number === 2) {
+    return "Paper";
+  } else if (number === 3) {
+    return "Scissors";
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+  const a =
+    playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+  const b = computerSelection;
+  if (a === b) {
+    return `Both are ${b}, let's try again.`;
+  } else if (
+    (a === "Scissors" && b === "Rock") ||
+    (a === "Rock" && b === "Paper") ||
+    (a === "Paper" && b === "Scissors")
+  ) {
+    return `You lose! ${b} beats ${a}`;
+  }
+  return `You win! ${a} beats ${b}`;
+}
+
+function playGame() {
+  let a = 0;
+  let b = 0;
+  for (let i = 0; i < 5; i++) {
+    const computerSelection = getComputerChoice();
+    const playerSelection = prompt("Rock, Paper or Scissors?");
+    playRound(playerSelection, computerSelection).startsWith("You win!")
+      ? a++
+      : b++;
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  return a > b ? "You win" : "You lose";
+}
+
+console.log(playGame());
